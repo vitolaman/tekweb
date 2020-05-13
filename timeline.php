4730d985164cb9f4c4d2f9976ac07cad9f5b9020
michaelpramastama <michaelwidap@gmail.com>
2020-05-13 20:44:15 +0700

    Update timeline.php

--- timeline.php
+++ timeline.php
@@ -14,7 +14,7 @@
         <!-- navbar -->
         <nav class="navbar navbar-light bg-dark">
             <form class="form-inline">
-                <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+                <input class="form-control mr-md-2" type="search" placeholder="Search" aria-label="Search">
                 <button class="btn btn-outline-info my-2 my-sm-0" type="submit">Search</button>
             </form>
 
